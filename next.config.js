4bfe593d88897a20ef709ac0994373e661e8501c
gayoung <gayounglee57@gmail.com>
2022-10-10 12:10:59 +1300

    Remove unnecessary config file

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    reactStrictMode: true,
-    env: {
-        NEXT_PUBLIC_REACT_APP_RAPID_API_KEY: process.env.NEXT_PUBLIC_REACT_APP_RAPID_API_KEY,
-    },
-    publicRuntimeConfig: {
-        NEXT_PUBLIC_REACT_APP_RAPID_API_KEY: process.env.NEXT_PUBLIC_REACT_APP_RAPID_API_KEY,
-    }
-}
