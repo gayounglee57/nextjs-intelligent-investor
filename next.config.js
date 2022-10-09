2934471853b1913b539dad18a5ccec377d8dde7b
gayoung <gayounglee57@gmail.com>
2022-10-09 20:30:47 +1300

    Modify config for api key and add margin for body of html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     reactStrictMode: true,
     env: {
-        NEXT_PUBLIC_REACT_APP_RAPID_API_KEY: process.env.NEXT_PUBLIC_REACT_APP_RAPID_API_KEY,
+        REACT_APP_RAPID_API_KEY: process.env.NEXT_PUBLIC_REACT_APP_RAPID_API_KEY,
     }
 }
